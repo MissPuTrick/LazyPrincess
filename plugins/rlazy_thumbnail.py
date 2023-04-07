736574e2beecee986e0ee414441797fda3be50aa
PuTrick <telmovid.cs@gmail.com>
2023-04-07 12:19:37 +0700

    Update rlazy_thumbnail.py

--- plugins/rlazy_thumbnail.py
+++ plugins/rlazy_thumbnail.py
@@ -15,16 +15,16 @@ async def viewthumb(client, message):
 	   chat_id=message.chat.id, 
 	   photo=thumb)
     else:
-        await message.reply_text("😔**Sorry ! No thumbnail found...**😔") 
+        await message.reply_text("😔**Maaf! Tidak ada thumbnail yang ditemukan...**😔") 
 		
 @Client.on_message(filters.private & filters.command(['delthumb']))
 async def removethumb(client, message):
     await db.set_thumbnail(message.from_user.id, file_id=None)
-    await message.reply_text("**Thumbnail deleted successfully**✅️")
+    await message.reply_text("**Thumbnail berhasil dihapus**✅️")
 	
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
-    LazyDev = await message.reply_text("Please Wait ...")
+    LazyDev = await message.reply_text("Harap tunggu...")
     await db.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
-    await LazyDev.edit("**Thumbnail saved successfully**✅️")
+    await LazyDev.edit("**Thumbnail berhasil disimpan**✅️")
 	
